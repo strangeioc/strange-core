0f11b903b0d3303190b02f842670d685e0641430
Pauli Savolainen <pauli.savolainen@greenergrass.company>
2015-01-31 15:46:27 +0200

    First implementation of the list binder with tests

---
+++ .tests/extensions/listBinding/TestListBinder.cs
@@ -0,0 +1,108 @@
+﻿using NUnit.Framework;
+using strange.extensions.context.impl;
+using strange.extensions.listBind.api;
+using strange.extensions.listBind.impl;
+using System.Collections.Generic;
+
+namespace strange.unittests
+{
+    [TestFixture]
+    public class TestListBinder
+    {
+        private MockContext context;
+        IListBinder binder;
+        private object contextView;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Context.firstContext = null;
+            contextView = new object();
+            context = new MockContext(contextView, true);
+            context.Start();
+            binder = context.listBinder;
+
+        }
+
+        [Test]
+        public void TestInjectListWithValues()
+        {
+            binder.Bind<string>().ToValue("a");
+            binder.Bind<string>().ToValue("b");
+            binder.Bind<string>().ToValue("c");
+
+            var strings = context.injectionBinder.GetInstance<List<string>>();
+
+            Assert.AreEqual(3, strings.Count);
+            Assert.AreEqual("a", strings[0]);
+            Assert.AreEqual("b", strings[1]);
+            Assert.AreEqual("c", strings[2]);
+        }
+
+        [Test]
+        public void TestInjectTypesToList()
+        {
+            binder.Bind<IListItem>().To<ListItemA>();
+            binder.Bind<IListItem>().To<ListItemB>();
+
+            var list = context.injectionBinder.GetInstance<List<IListItem>>();
+
+            Assert.AreEqual(2, list.Count);
+            Assert.IsAssignableFrom(typeof(ListItemA), list[0]);
+            Assert.IsAssignableFrom(typeof(ListItemB), list[1]);
+        }
+
+
+        [Test]
+        public void TestInjectListWithSingletonItems()
+        {
+            binder.Bind<IListItem>().To<ListItemA>().ToSingleton();
+            binder.Bind<IListItem>().To<ListItemB>();
+
+            var list1 = context.injectionBinder.GetInstance<List<IListItem>>();
+            var list2 = context.injectionBinder.GetInstance<List<IListItem>>();
+
+            Assert.AreNotEqual(list1, list2);
+
+            // Singleton
+            Assert.AreEqual(list1[0], list2[0]);
+
+            // New instance
+            Assert.AreNotEqual(list1[1], list2[1]);
+
+        }
+
+        [Test]
+        public void TestInjectListWithMixedValuesAndTypes()
+        {
+            binder.Bind<IListItem>().ToValue(new ListItemA());
+            binder.Bind<IListItem>().To<ListItemB>();
+
+            var list1 = context.injectionBinder.GetInstance<List<IListItem>>();
+            var list2 = context.injectionBinder.GetInstance<List<IListItem>>();
+
+            Assert.AreNotEqual(list1, list2);
+
+            // Singleton
+            Assert.AreEqual(list1[0], list2[0]);
+
+            // New instance
+            Assert.AreNotEqual(list1[1], list2[1]);
+
+        }
+    }
+
+    public interface IListItem
+    {
+
+    }
+
+    public class ListItemA : IListItem
+    {
+
+    }
+    public class ListItemB: IListItem
+    {
+
+    }
+}
---
+++ extensions/listBinding/impl/ListBinding.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Copyright 2013 ThirdMotion, Inc.
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *		Unless required by applicable law or agreed to in writing, software
+ *		distributed under the License is distributed on an "AS IS" BASIS,
+ *		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *		See the License for the specific language governing permissions and
+ *		limitations under the License.
+ */
+
+using strange.extensions.injector.api;
+using strange.extensions.listBind.api;
+using strange.framework.api;
+using strange.framework.impl;
+using System;
+using System.Collections.Generic;
+
+namespace strange.extensions.listBind.impl
+{
+    public class ListBinding : Binding, IListBinding
+    {
+        private readonly List<IInjectionBinding> bindings = new List<IInjectionBinding>();
+
+        private readonly IInjectionBinder injectionBinder;
+
+        public ListBinding(Binder.BindingResolver resolver, IInjectionBinder injectionBinder)
+            : base(resolver)
+		{
+            this.injectionBinder = injectionBinder;
+		}
+
+        public Type ListType { get; set; }
+
+        public List<IInjectionBinding> Bindings { get { return this.bindings;  } }
+
+        public new IInjectionBinding To<T>()
+        {
+            var binding = injectionBinder.Bind(ListType).To<T>().ToName(typeof(T).ToString());
+            bindings.Add(binding);
+            //var binding = base.To<T>().ToName(typeof(T).ToString());
+            //this.bindings.Add(binding);
+            return binding;
+
+        }
+
+        public IInjectionBinding ToValue(object o)
+        {
+            var binding = injectionBinder.Bind(ListType).ToValue(o).ToName(o.ToString());
+            bindings.Add(binding);
+            return binding;
+        }
+
+    }
+}
